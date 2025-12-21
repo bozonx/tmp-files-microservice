abb0c4f2060e97dd4a4484928c86d9c1cd2c58e8
Ivan <ipkozyrin@gmail.com>
2025-12-21 12:57:36 -0300

    fix

--- src/common/constants/app.constants.ts
+++ src/common/constants/app.constants.ts
@@ -1,5 +1,5 @@
 // Timeout for app.close() during graceful shutdown (60 seconds)
-export const APP_CLOSE_TIMEOUT_MS = 60000
+export const APP_CLOSE_TIMEOUT_MS = 25000
 
 // Maximum number of files to process in a single cleanup batch
 export const CLEANUP_BATCH_SIZE = 1000
